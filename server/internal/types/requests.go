767fed3d2fbfc2526d76751dca31f779753a2912
Jackson Furr <jackson.a.furr@gmail.com>
2024-09-19 22:41:41 -0400

    /register handles role-related db actions

--- server/internal/types/requests.go
+++ server/internal/types/requests.go
@@ -4,12 +4,3 @@ type RequestPayloadLogin struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
-
-type RequestPayloadRegister struct {
-	Username  string `json:"username"`
-	Password  string `json:"password"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Role      string `json:"role"`
-	TeacherID string `json:"teacher_id,omitempty"`
-}
