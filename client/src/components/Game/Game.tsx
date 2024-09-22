f7a9a411ed8e61a0ea5b4b000b2118eb68af8d2f
Jackson Furr <jackson.a.furr@gmail.com>
2024-09-22 15:26:51 -0400

    Game holds game status and settings

--- client/src/components/Game/Game.tsx
+++ client/src/components/Game/Game.tsx
@@ -1,23 +1,37 @@
 import styles from "./Game.module.scss";
 import { useState } from "react";
 import Settings from "./children/Settings/Settings";
-import { E_GAME_STATUS } from "../../types/game";
+import {
+  E_GAME_STATUS,
+  INIT_GAME_SETTINGS,
+  T_GAME_SETTINGS,
+} from "../../types/game";
+import { deepCopyObject } from "../../utils/methods";
 
 const Game: React.FC = () => {
   const [gameStatus, setGameStatus] = useState<E_GAME_STATUS>(
     E_GAME_STATUS.PRE,
   );
+  const [gameSettings, setGameSettings] = useState<T_GAME_SETTINGS>(
+    deepCopyObject(INIT_GAME_SETTINGS),
+  );
 
-	switch (gameStatus) {
-		case E_GAME_STATUS.PRE:
-			return (
-				<div className={styles.root}>
-					<Settings />
-				</div>
-			);
-		case E_GAME_STATUS.ACTIVE:
-		case E_GAME_STATUS.POST:
-	}
+  console.log(gameSettings);
+
+  switch (gameStatus) {
+    case E_GAME_STATUS.PRE:
+      return (
+        <div className={styles.root}>
+          <Settings
+            setGameSettings={setGameSettings}
+            setGameStatus={setGameStatus}
+          />
+        </div>
+      );
+    case E_GAME_STATUS.ACTIVE:
+      return <div className={styles.root}>Active</div>;
+    case E_GAME_STATUS.POST:
+  }
 };
 
 export default Game;
