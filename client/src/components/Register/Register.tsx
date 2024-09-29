444f3f4b5169dc3ed7b8e1e6eb46fce7d6af79a6
Jackson Furr <jackson.a.furr@gmail.com>
2024-09-29 13:35:38 -0400

    init register types

--- client/src/components/Register/Register.tsx
+++ client/src/components/Register/Register.tsx
@@ -3,6 +3,46 @@ import Student from "./children/Student/Student";
 import Choose from "./children/Choose/Choose";
 import Teacher from "./children/Teacher/Teacher";
 
+export type T_FORM_REGISTER_USER = {
+  username: string;
+  password: string;
+  confirm_password: string;
+  first_name: string;
+  last_name: string;
+};
+export const INIT_FORM_REGISTER_USER: T_FORM_REGISTER_USER = {
+  username: "",
+  password: "",
+  confirm_password: "",
+  first_name: "",
+  last_name: "",
+};
+
+export type T_FORM_REGISTER_STUDENT = {
+  teacher_id: number;
+  class_id: number;
+};
+export const INIT_FORM_REGISTER_STUDENT: T_FORM_REGISTER_STUDENT = {
+  teacher_id: -1,
+  class_id: -1,
+};
+
+export type T_FORM_REGISTER_TEACHER = {
+  email: string;
+  school: string;
+  classes: T_CLASS[];
+};
+export const INIT_FORM_REGISTER_TEACHER: T_FORM_REGISTER_TEACHER = {
+  email: "",
+  school: "",
+  classes: [],
+};
+
+export type T_CLASS = {
+  name: string;
+  class_id: number;
+};
+
 const Register: React.FC = () => {
   return (
     <>
