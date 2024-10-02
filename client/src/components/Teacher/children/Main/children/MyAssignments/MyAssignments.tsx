4176ceabc6d4a7e2886da468dee004bd8edd9fdd
Jackson Furr <jackson.a.furr@gmail.com>
2024-10-01 22:05:15 -0400

    created Teacher/MyAssignments

---
+++ client/src/components/Teacher/children/Main/children/MyAssignments/MyAssignments.tsx
@@ -0,0 +1,7 @@
+import styles from "./MyAssignments.module.scss";
+
+const MyAssignments: React.FC = () => {
+	return <div className={styles.root}>MyAssignments</div>;
+};
+
+export default MyAssignments;
