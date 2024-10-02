63d5fd16b70556625572e2dc546b0bbbda88890a
Jackson Furr <jackson.a.furr@gmail.com>
2024-10-01 22:43:44 -0400

    added Assignment/NewAssignment

---
+++ client/src/components/Teacher/children/Main/children/MyAssignments/children/NewAssignment/NewAssignment.tsx
@@ -0,0 +1,7 @@
+import styles from "./NewAssignment.module.scss";
+
+const NewAssignment: React.FC = () => {
+	return <div className={styles.root}>Settings</div>;
+};
+
+export default NewAssignment;
