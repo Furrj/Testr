34a48fedb53b4f1e6be10b3e419831a8c41f0f7f
Jackson Furr <jackson.a.furr@gmail.com>
2024-10-21 14:46:55 -0400

    settings page

--- client_remake/src/components/Settings/Settings.tsx
+++ client_remake/src/components/Settings/Settings.tsx
@@ -1,7 +1,26 @@
 import styles from "./Settings.module.scss";
 
 const Settings: React.FC = () => {
-	return <div className={styles.root}>Settings</div>;
+	return (
+		<section className={styles.root}>
+			<div className={styles.scroll}>
+				<section className={styles.content}>
+					<div>
+						<h1>Settings</h1>
+					</div>
+					<div>
+						<h2>Game</h2>
+						<div className={styles.inputs}>
+							<div>
+								<label htmlFor="vertical">Vertical Layout</label>
+								<input type="checkbox" name="vertical" id="vertical" />
+							</div>
+						</div>
+					</div>
+				</section>
+			</div>
+		</section>
+	);
 };
 
 export default Settings;
