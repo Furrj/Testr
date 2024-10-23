f02f0981d8c569bd68ff74ff795d303f7e88aded
Jackson Furr <jackson.a.furr@gmail.com>
2024-10-23 00:30:32 -0400

    removed original client

--- client_remake/prettier.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-prettier --write . | grep -v "(unchanged)"
--- client_remake/src/utils/inputHandlers.ts
+++
@@ -1,6 +0,0 @@
-export function handleInput(
-	e: React.ChangeEvent<HTMLInputElement>,
-	callback: React.Dispatch<React.SetStateAction<string>>,
-) {
-	callback(e.target.value);
-}
