c4c59a87edaed0194cb1d8b1bb10b0001415cf0b
Jackson Furr <jackson.a.furr@gmail.com>
2024-10-24 23:50:37 -0400

    sql migration updated

--- config/sql/migration_v3/migrate.sql
+++ config/sql/migration_v3/migrate.sql
@@ -8,7 +8,7 @@ CREATE TABLE teachers.registration
     user_id INTEGER REFERENCES users.ids (user_id) ON DELETE CASCADE,
     email   TEXT UNIQUE,
     code    UUID,
-    expiry  BIGINT DEFAULT EXTRACT(EPOCH FROM (NOW() + INTERVAL '24 hours'))
+    expiry  BIGINT DEFAULT EXTRACT(EPOCH FROM (NOW() + INTERVAL '1 hour'))::BIGINT
 );
 
 COMMIT;
