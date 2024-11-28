321858d061a0588f691c0ae53eb5e156c9179bbe
Jackson Furr <jackson.a.furr@gmail.com>
2024-11-28 12:27:50 -0500

    refactor Jwt service

--- server2/internal/services/jwt/jwt.go
+++ server2/internal/services/jwt/jwt.go
@@ -1,21 +1,55 @@
 package jwt
 
 import (
-	"github.com/Furrj/timestrainer/server/internal/api"
-	"github.com/Furrj/timestrainer/server/internal/services/env"
+	"errors"
+	"fmt"
+
+	"github.com/golang-jwt/jwt/v5"
 )
 
 type JwtManager[T any] interface {
-	Create(T) (api.Jwt, error)
-	Parse(api.Jwt) (T, error)
+	Create(T) (Jwt, error)
+	Parse(Jwt) (T, error)
 }
 
-type Jwts struct {
-	User JwtManager[JwtFieldsUser]
+type (
+	Jwts struct {
+		Secret []byte
+	}
+	Jwt       = string
+	timestamp = int64
+)
+
+func (jm Jwts) Create(t *jwt.Token) (Jwt, error) {
+	s, err := t.SignedString(jm.Secret)
+	if err != nil {
+		return "", errors.New("error creating signed string")
+	}
+
+	return s, nil
 }
 
-func NewJwts(env env.EnvVars) Jwts {
-	return Jwts{User: newUser(env)}
+func (jm Jwts) Parse(j Jwt) (*jwt.Token, error) {
+	// Parse the token
+	token, err := jwt.Parse(j, func(token *jwt.Token) (interface{}, error) {
+		// Make sure that the token's algorithm matches the expected algorithm
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(jm.Secret), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// Validate the token
+	if !token.Valid {
+		return nil, fmt.Errorf("invalid token")
+	}
+
+	return token, nil
 }
 
-type timestamp = int64
+func newJwtManager(secret []byte) JwtManager[*jwt.Token] {
+	return Jwts{Secret: secret}
+}
